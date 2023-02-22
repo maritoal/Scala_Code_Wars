57bc1cfdf79792a94f3653048430158299475e04
maritoal <mariorasgado@hotmail.com>
2023-02-22 14:41:48 -0500

    First version of code

---
+++ src/main/scala/SquaringAnArgument.scala
@@ -0,0 +1,3 @@
+object SquaringAnArgument extends App{
+  def square(n: Int): Int = Math.pow(n,2).toInt
+}
---
+++ src/test/scala/SquaringAnArgumentTest.scala
@@ -0,0 +1,6 @@
+ import org.scalatest.funsuite.AnyFunSuite
+ class SquaringAnArgumentTest extends AnyFunSuite{
+   test("CubeCalculator.cube") {
+     assert(SquaringAnArgument.square(2) === 4)
+   }
+}
